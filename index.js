f63792962079fed721934feb07e066e3845e241b
Krisztian Holub <kholub1989@gmail.com>
2020-03-23 15:07:22 -0400

    fetching movie data

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '1b89a364',
+      s: 'avengers'
+    }
+  });
+  console.log(response.data);
+};
+
+fetchData();
