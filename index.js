67849d2058e821a18990c89b7aef5b895f9d5377
Krisztian Holub <kholub1989@gmail.com>
2020-03-24 20:11:27 -0400

    extracting utility functions

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ const fetchData = async (serachTerm) => {
   console.log(response.data);
 };
 
-
 const input = document.querySelector('input');
-
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if(timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
 
 const onInput = (event) => {
   fetchData(event.target.value);
