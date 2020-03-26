500230186800d0c7001447b5d60ec468da63e150
Krisztian Holub <kholub1989@gmail.com>
2020-03-26 13:00:16 -0400

    handling movie selection

--- index.js
+++ index.js
@@ -45,7 +45,10 @@ const onInput = async (event) => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
-
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
     resultsWrapper.appendChild(option);
   }
 };
