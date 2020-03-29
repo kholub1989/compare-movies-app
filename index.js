11931f5f38fa8cfde67eb2bf19634fbe952069d7
Krisztian Holub <kholub1989@gmail.com>
2020-03-29 09:59:25 -0400

    removing movie references

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const fetchData = async (serachTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '1b89a364',
-      s: serachTerm
-    }
-  });
-  if(response.data.Error) {
-    return [];
-  }
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector('.autocomplete'),
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    
     return `
       <img src="${imgSrc}" />
       ${movie.Title}
@@ -26,6 +14,20 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(serachTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '1b89a364',
+        s: serachTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
   }
 }); 
 
