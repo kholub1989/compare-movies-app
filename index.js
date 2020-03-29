09f0543191eba7b0ed7550fd6e3b1c745a27f83f
Krisztian Holub <kholub1989@gmail.com>
2020-03-29 09:37:00 -0400

    extracting rendering logic

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ const fetchData = async (serachTerm) => {
 };
 
 createAutoComplete({
-  root: document.querySelector('.autocomplete')
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+      (${movie.Year})
+    `;
+  }
 }); 
-createAutoComplete({
-  root: document.querySelector('.autocomplete-two')
-});
-createAutoComplete({
-  root: document.querySelector('.autocomplete-three')
-});
 
 const onMovieSelect = async (movie) => {
   const response = await axios.get('http://www.omdbapi.com/', {
